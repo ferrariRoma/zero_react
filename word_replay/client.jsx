1a06c566c39412b362a43e4e98102b13df4cf472
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2022-07-03 01:46:41 +0900

    Update: webpack hot reloading and devServer

--- word_replay/client.jsx
+++ word_replay/client.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM, { createRoot } from "react-dom/client";
 import WordReplay from "./wordReplay";
 
-ReactDOM.render(<WordReplay />, document.querySelector("#root"));
+// ReactDOM.render(<WordReplay />, document.querySelector("#root"));
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <>
+    <WordReplay />
+  </>
+);
--- word_replay/wordReplay.jsx
+++ word_replay/wordReplay.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import { useState, useRef } from "react";
+import { render } from "react-dom";
 
 const WordReplay = () => {
-  return <div>Hello world replay</div>;
+  const [word, setWord] = useState("제로초");
+  const [value, setValue] = useState("");
+  const [result, setResult] = useState("");
+  const onRefInput = useRef(null);
+
+  const onChangeInput = (e) => {
+    return setValue(e.target.value);
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (word[word.length - 1] === value[0]) {
+      setWord(value);
+      setValue("");
+      setResult(`${word}에서 ${value}로 성공!`);
+      onRefInput.current.focus();
+    } else {
+      setValue("");
+      setResult(`${word}에서 ${value}? 땡!!`);
+      onRefInput.current.focus();
+    }
+  };
+
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          onChange={onChangeInput}
+          ref={onRefInput}
+          value={value}
+        />
+        <button type="submit">제출</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
 };
 
 export default WordReplay;
+/* 
+컨트롤드 인풋, 언컨트롤드 인풋
+onSubmit에서만 input을 쓰는 경우에는 value나 onChange을 안해줘도 됨
+e.target.children.word
+
+근데 그냥 컨트롤드 인풋만 써도 된다.
+*/
