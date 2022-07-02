fbe4db323c5add3bf44df93f1fe372b891e655dd
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2022-07-02 20:07:51 +0900

    Set: webpack word_replay

---
+++ word_replay/client.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import WordReplay from "./wordReplay";
+
+ReactDOM.render(<WordReplay />, document.querySelector("#root"));
---
+++ word_replay/wordReplay.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useState, useRef } from "react";
+
+const WordReplay = () => {
+  return <div>Hello world replay</div>;
+};
+
+export default WordReplay;
